bdca09e04a956598ea424483a29d91d05f34f5ad
aadharsh1994-bit <aadharsh1994@gmail.com>
2025-09-18 07:18:07 -0700

    Add service worker for PWA support

---
+++ public/service-worker.js
@@ -0,0 +1,24 @@
+// public/service-worker.js
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/icon-192.png',
+    '/icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
